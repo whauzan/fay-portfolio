5d023cd9fa1c5e8cffb3fd2dbb4f7c7cf8a4d92b
Wahyu Hauzan Rafi <whauzan@air.local>
2023-11-14 01:45:09 +0700

    style: delete commented code

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -6,33 +6,6 @@ import Icon from "../shared/Icon";
 
 const HeroSection = () => {
   return (
-    // <section className="relative h-[calc(100vh-110px)]">
-    //   <div className="absolute left-1/2 top-1/2 w-7/12 -translate-x-1/2 translate-y-[-90%]  whitespace-nowrap font-catamaran text-[4rem] font-medium leading-none">
-    //     <h2 className="flex items-center gap-x-8">
-    //       <span className="text-[10rem] text-brand-blue">Hi </span>I&apos;m
-    //       Aisyah, and
-    //     </h2>
-    //     <div className="ml-[18rem]">
-    //       <span className="mr-6">I do</span>
-    //       <TextLoop texts={["graphic design", "UI/UX design", "game & art"]} />
-    //     </div>
-    //   </div>
-    //   <LinkScroll href="about-me">
-    //     <div className="absolute bottom-32 left-[48%] flex -translate-x-1/2 animate-bounce flex-col items-center justify-end text-base font-bold text-brand-red">
-    //       <p>scroll down</p>
-    //       <svg
-    //         width="24"
-    //         height="24"
-    //         viewBox="0 0 24 24"
-    //         fill="currentColor"
-    //         xmlns="http://www.w3.org/2000/svg"
-    //       >
-    //         <path d="M18 6.41L16.59 5L12 9.58L7.41 5L6 6.41L12 12.41L18 6.41Z" />
-    //         <path d="M18 13L16.59 11.59L12 16.17L7.41 11.59L6 13L12 19L18 13Z" />
-    //       </svg>
-    //     </div>
-    //   </LinkScroll>
-    // </section>
     <section className="relative h-screen">
       <Image
         src={"/bg-home-top.svg"}
